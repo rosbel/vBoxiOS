9d4aba16714b2ff5f70fa23d2b9155c903bc2ad4
rlsanroman <rlsanroman@gmail.com>
2014-10-02 17:42:37 -0500

    Initial Commit

---
+++ vBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vBox
+//
+//  Created by Rosbel Sanroman on 10/2/14.
+//  Copyright (c) 2014 rosbelSanroman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
