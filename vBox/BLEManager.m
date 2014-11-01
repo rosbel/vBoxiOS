a41c3ff87e88d8a6119aabf8054b12a5dd3fd960
rlsanroman <rlsanroman@gmail.com>
2014-11-01 12:24:20 -0500

    Fixed Bluetooth With CheckSum

--- vBox/BLEManager.m
+++ vBox/BLEManager.m
@@ -39,7 +39,7 @@
 
 @property (nonatomic, strong, readonly) CBCentralManager *centralManager;
 @property (nonatomic, strong, readonly) CBPeripheral *peripheral;
-
+@property (nonatomic, strong, readonly) CBUUID *uid;
 @end
 
 #pragma mark - Implementation 
@@ -52,8 +52,6 @@
 		uint8_t checksum;
 		float value[3];
 	};
-	
-	CBUUID *uid;
 }
 
 
@@ -84,14 +82,14 @@
 	switch(type)
 	{
 		case PeripheralTypeOBDAdapter:
-			uid = [CBUUID UUIDWithString:OBDAdapterServiceUID];
+			_uid = [CBUUID UUIDWithString:OBDAdapterServiceUID];
 			break;
 		case PeripheralTypeBeagleBone:
-			uid = [CBUUID UUIDWithString:BeagleBoneServiceUID];
+			_uid = [CBUUID UUIDWithString:BeagleBoneServiceUID];
 			break;
 	}
 	[self.delegate didUpdateDebugLogWithString:@"Scanning for peripheral"];
-	[self.centralManager scanForPeripheralsWithServices:@[uid] options:nil];
+	[self.centralManager scanForPeripheralsWithServices:@[self.uid] options:nil];
 	if([self.delegate respondsToSelector:@selector(didBeginScanningForPeripheral)])
 		[self.delegate didBeginScanningForPeripheral];
 	return YES;
@@ -161,7 +159,11 @@
 
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
 {
-	[self.peripheral discoverServices:nil]; //Discover all services
+//	[self.peripheral discoverServices:nil]; //Discover all services
+	if(peripheral == self.peripheral)
+		NSLog(@"SAMEONE");
+	[peripheral setDelegate:self];
+	[peripheral discoverServices:nil];
 	if([self.delegate respondsToSelector:@selector(didConnectPeripheral)])
 //		[self asyncToMainThread:^{
 			[self.delegate didConnectPeripheral];
@@ -193,7 +195,7 @@
 		_peripheral = peripheral;
 		_peripheral.delegate = self;
 		
-		[self.centralManager connectPeripheral:_peripheral options:@{CBConnectPeripheralOptionNotifyOnDisconnectionKey:@YES,CBConnectPeripheralOptionNotifyOnDisconnectionKey:@YES}];
+		[self.centralManager connectPeripheral:peripheral options:@{CBConnectPeripheralOptionNotifyOnDisconnectionKey:@YES,CBConnectPeripheralOptionNotifyOnDisconnectionKey:@YES}];
 	}
 }
 
@@ -223,6 +225,7 @@
 	for(CBCharacteristic *characteristic in service.characteristics)
 	{
 		[peripheral setNotifyValue:YES forCharacteristic:characteristic];
+//		[peripheral readValueForCharacteristic:characteristic];
 //		[self asyncToMainThread:^{
 			[self.delegate didUpdateDebugLogWithString:[NSString stringWithFormat:@"Characteristic UUID = %@",characteristic.UUID]];
 //		}];
@@ -235,97 +238,113 @@
 	struct BLE_DATA receivedData12;
 	[characteristic.value getBytes:&receivedData12 length:12];
 	[characteristic.value getBytes:&receivedData length:20];
+	uint8_t prevCheckSum12 = receivedData12.checksum;
+	uint8_t prevCheckSum = receivedData.checksum;
+	receivedData12.checksum = 0;
+	receivedData.checksum = 0;
+	uint8_t checkSum12 = [self getCheckSum:&receivedData12 length:12];
+	uint8_t checkSum = [self getCheckSum:&receivedData length:20];
 	
-	uint8_t checkSum12 = [self getCheckSum:&receivedData12 size:12];
-	uint8_t checkSum20 = [self getCheckSum:&receivedData size:20];
+	struct BLE_DATA correctData;
 	
+	if(prevCheckSum == checkSum)
+	{
+		correctData = receivedData;
+	}
+	else if(prevCheckSum12 == checkSum12)
+	{
+		correctData = receivedData12;
+	}else
+	{
+		return;
+	}
 	float value;
-	switch(receivedData.pid)
+	switch(correctData.pid)
 	{
 		case PID_FUEL_LEVEL:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Fuel" withValue:value ifUnderLimit:150.0];
 			break;
 		case PID_RPM:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"RPM" withValue:value ifUnderLimit:100000.0];
 			break;
 		case PID_RUNTIME:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Runtime" withValue:value ifUnderLimit:FLT_MAX]; //maybe change
 			break;
 		case PID_ENGINE_TORQUE_PERCENTAGE:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Engine Torque Percentage" withValue:value ifUnderLimit:150]; //check
 			break;
 		case PID_ENGINE_LOAD:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Engine Load" withValue:value ifUnderLimit:150]; //check
 			break;
 		case PID_DISTANCE:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Distance" withValue:value ifUnderLimit:10000000.0]; //check
 			break;
 		case PID_COOLANT_TEMP:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Coolant Temp" withValue:value ifUnderLimit:500.0];
 			break;
 		case PID_BAROMETRIC:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Barometric" withValue:value ifUnderLimit:500.0];
 			break;
 		case PID_SPEED:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Speed" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_ENGINE_FUEL_RATE:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Engine Fuel Rate" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_AMBIENT_TEMP:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Ambien Temp" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_THROTTLE:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Throttle" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_INTAKE_TEMP:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"Intake Temp" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_GPS_ALTITUDE:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"GPS Alt" withValue:value ifUnderLimit:FLT_MAX];
 			break;
 		case PID_GPS_LATITUDE:
-			value = receivedData.value[0];
-			value /= 100; //offset of 100
+			value = correctData.value[0];
+//			value /= 100; //offset of 100
 			[self asyncUpdateDiagnosticForKey:@"GPS Lat" withValue:value ifUnderLimit:FLT_MAX];
 			break;
 		case PID_GPS_LONGITUDE:
-			value = receivedData.value[0];
-			value /= 100;
+			value = correctData.value[0];
+//			value /= 100;
 			[self asyncUpdateDiagnosticForKey:@"GPS Long" withValue:value ifUnderLimit:FLT_MAX];
 			break;
 		case PID_GPS_HEADING:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"GPS Heading" withValue:value ifUnderLimit:FLT_MAX];
 			break;
 		case PID_GPS_SAT_COUNT:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"GPS Sat Count" withValue:value ifUnderLimit:FLT_MAX];
 			break;
 		case PID_GPS_SPEED:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"GPS Speed" withValue:value ifUnderLimit:1000.0];
 			break;
 		case PID_GPS_TIME:
-			value = receivedData.value[0];
+			value = correctData.value[0];
 			[self asyncUpdateDiagnosticForKey:@"GPS Time" withValue:value ifUnderLimit:1000000.0];
 			break;
 		case PID_ACC:
-			[self asyncUpdateDiagnosticForKey:@"Accelerometer" withMultipleValues:receivedData.value]; //ALL 3 values are needed
+			[self asyncUpdateDiagnosticForKey:@"Accelerometer" withMultipleValues:correctData.value]; //ALL 3 values are needed
 			break;
 		case 0:
 			//ignore
@@ -336,7 +355,7 @@
 		default:
 			if([self.delegate respondsToSelector:@selector(didUpdateDebugLogWithString:)])
 //				[self asyncToMainThread:^{
-					[self.delegate didUpdateDebugLogWithString:[NSString stringWithFormat:@"Unkown PID: %x - Val: %f",receivedData.pid,receivedData.value[0]]];
+					[self.delegate didUpdateDebugLogWithString:[NSString stringWithFormat:@"Unkown PID: %x - Val: %f",correctData.pid,correctData.value[0]]];
 //				}];
 			break;
 	}
@@ -365,7 +384,7 @@
 -(void) asyncUpdateDiagnosticForKey:(NSString *)key withMultipleValues:(float[])values
 {
 //	[self asyncToMainThread:^{
-	NSArray *array = @[[NSNumber numberWithFloat:values[0]],[NSNumber numberWithFloat:values[0]],[NSNumber numberWithFloat:values[0]]];
+	NSArray *array = @[[NSNumber numberWithFloat:values[0]],[NSNumber numberWithFloat:values[1]],[NSNumber numberWithFloat:values[2]]];
 	[self.delegate didUpdateDiagnosticForKey:key withMultipleValues:array];
 //	}];
 }
@@ -375,9 +394,9 @@
 	dispatch_async(dispatch_get_main_queue(), codeBlock);
 }
 
-#pragma mark - Checksum
+#pragma mark - CheckSum For Correct Bluetooth Data
 
--(uint8_t)getCheckSum:(char *)buffer size:(size_t)len
+-(uint8_t) getCheckSum:(char *)buffer length:(Byte)len
 {
 	uint8_t checksum = 0;
 	for (Byte i = 0; i < len; i++) {
